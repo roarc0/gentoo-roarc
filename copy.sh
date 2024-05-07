d8f0bce68ed4ff9afd9fd2e592d0aebb62c76098
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2024-05-07 07:53:23 +0200

    add copy.sh to copy ebuild from /usr/portage

---
+++ copy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+if [ -z "$1" ]; then
+    echo "No argument supplied. Please provide a folder name."
+    exit 1
+fi
+
+src_dir="/usr/portage/$1"
+if [ ! -d "$src_dir" ]; then
+    echo "Source directory '$src_dir' does not exist."
+    exit 1
+fi
+
+dest_dir="./$1"
+
+mkdir -p $dest_dir
+cp -r $src_dir/* $dest_dir
+
+find "$dest_dir" -name 'Manifest' -delete
+find "$dest_dir" -name 'metadata.xml' -delete
+
